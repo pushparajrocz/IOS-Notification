109b99c11ca33f0a072188e18f96c94b5d737945
Pushparaj S.P.R <37431391+pushparajrocz@users.noreply.github.com>
2020-11-28 04:21:47 +0530

    Update ViewController.swift
    comment

--- Notificaiton/Notificaiton/ViewController.swift
+++ Notificaiton/Notificaiton/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // getting current badge number from UIApplication
     var badge = UIApplication.shared.applicationIconBadgeNumber
     
     override func viewDidLoad() {
@@ -21,23 +22,30 @@ class ViewController: UIViewController {
         let subTitle = "go ahead"
         let body = "U are Notified"
         badge += 1
+        // user build function which configure notification
         Notify(title, subTitle, body, badge)
         
     }
     
     func Notify(_ title: String, _ subTitle: String, _ body: String, _ badge: Int)
     {
+        // trigger identifies when and how should notification shown
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        // actual content of the notification
         let content = UNMutableNotificationContent()
         content.title = title
         content.subtitle = subTitle
         content.body = body
         content.badge = NSNumber(integerLiteral: badge)
         
+        // unique string id
         let uuid = UUID().uuidString
         
+        // request for notification service 
         let request = UNNotificationRequest(identifier: uuid, content: content, trigger: trigger)
         
+        // finally add the request to the notification center
         UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
     }
 }
